9b7d9b8efd91a7e865d3bb3cb99da340844db0ff
zhoujck <114135342+zhoujck@users.noreply.github.com>
2022-12-08 17:33:43 +0800

    Update diy-j.sh

--- diy-j.sh
+++ diy-j.sh
@@ -31,7 +31,7 @@ mv $CURRENT_DIR/DIY/app_icon.png $CURRENT_DIR/$DIR/app/src/main/res/drawable-xxx
 
 #主页微调
 cp $CURRENT_DIR/DIY/设置/fragment_user.xml $CURRENT_DIR/$DIR/app/src/main/res/layout/fragment_user.xml
-cp $CURRENT_DIR/DIY/设置/UserFragment.java $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/ui/fragment/UserFragment.java
+#cp $CURRENT_DIR/DIY/设置/UserFragment.java $CURRENT_DIR/$DIR/app/src/main/java/com/github/tvbox/osc/ui/fragment/UserFragment.java
 
 #背景修改
 cp $CURRENT_DIR/DIY/app_bg_black.png $CURRENT_DIR/$DIR/app/src/main/res/drawable/app_bg.png
